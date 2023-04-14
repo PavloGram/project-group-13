6236cf64c720f58d5ebcba2c231ba580d9cd68f4
khvadim69 <101789017+khvadim69@users.noreply.github.com>
2023-04-14 19:53:04 +0300

    добавляє файли сниппера
    Co-Authored-By: PavloGram <113470072+PavloGram@users.noreply.github.com>

---
+++ src/spinner.js
@@ -0,0 +1,12 @@
+window.onload = function () {
+    const $body = document.body;
+    const $preloader = $body.querySelector('.preloader');
+    function afterTransition() {
+      $body.classList.add('loaded');
+      $body.classList.remove('loaded_hiding');
+      $body.classList.remove('overflow_hidden');
+      $preloader.removeEventListener('transitionend', afterTransition);
+    }
+    $body.classList.add('loaded_hiding');
+    $preloader.addEventListener('transitionend', afterTransition);
+  };
