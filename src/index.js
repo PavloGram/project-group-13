3b1f1178d4fac801663e0d3a7ae12ae4ef31cbcf
PavloGram <113470072+PavloGram@users.noreply.github.com>
2023-04-15 09:11:58 +0300

    add search of query

--- src/index.js
+++ src/index.js
@@ -1,22 +1,26 @@
 import { fetchFilms } from './fetchFilms.js';
 const BASE_URL_TRENDS = 'https://api.themoviedb.org/3/trending/all/day';
+const BASE_URL_QUERY = 'https://api.themoviedb.org/3/search/movie';
 const API_KEY = '1962278b5026dd7c7bb0a91cd47f798b';
 
 const inputEl = document.querySelector('.input');
 
-const searchParamsToTrends = new URLSearchParams({
-  api_key: '1962278b5026dd7c7bb0a91cd47f798b',
-});
+inputEl.addEventListener('input', inputQuery);
 
-popularFilms();
-function popularFilms() {
-  const url = `${BASE_URL_TRENDS}?${searchParamsToTrends}`;
+function inputQuery(e) {
+  const input = e.target.value.trim();
+
+  const searchParamsToQuery = new URLSearchParams({
+    api_key: '1962278b5026dd7c7bb0a91cd47f798b',
+    query: input,
+  });
+  const url = `${BASE_URL_QUERY}?${searchParamsToQuery}`;
   fetchFilms(url)
     .then(forMarcup => {
       console.log(forMarcup);
-      // forMarcup це масив об’єктів популярних фільмів
+      // forMarcup це масив об’єктів фільмів за запитом
     })
     .catch(er => {
-      cvonsole.log(er);
+      console.log(er);
     });
 }
