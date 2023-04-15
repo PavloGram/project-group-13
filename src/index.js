f7180c76e206bb929d31f32b6e4745d13611ce2e
PavloGram <113470072+PavloGram@users.noreply.github.com>
2023-04-15 08:33:09 +0300

    і

--- src/index.js
+++ src/index.js
@@ -1,26 +1,22 @@
 import { fetchFilms } from './fetchFilms.js';
-const BASE_URL = 'https://api.themoviedb.org/3/movie/76341'
-const API_KEY = '1962278b5026dd7c7bb0a91cd47f798b'
+const BASE_URL_TRENDS = 'https://api.themoviedb.org/3/trending/all/day';
+const API_KEY = '1962278b5026dd7c7bb0a91cd47f798b';
 
-const inputEl = document.querySelector('.input')
+const inputEl = document.querySelector('.input');
 
-// const searchParams = new URLSearchParams({
-//     api_key: '1962278b5026dd7c7bb0a91cd47f798b',
-//     media_type: 'all',
-//     time_window: 'week',
+const searchParamsToTrends = new URLSearchParams({
+  api_key: '1962278b5026dd7c7bb0a91cd47f798b',
+});
 
-// });
-
-popularFilms()
+popularFilms();
 function popularFilms() {
-   
-    fetchFilms()
-      .then(forMarcup => {
-        console.log(forMarcup);
-        return marcupList(forMarcup);
-
-      })
-      .catch(er => {
-        
-      });
+  const url = `${BASE_URL_TRENDS}?${searchParamsToTrends}`;
+  fetchFilms(url)
+    .then(forMarcup => {
+      console.log(forMarcup);
+      // forMarcup це масив об’єктів популярних фільмів
+    })
+    .catch(er => {
+      cvonsole.log(er);
+    });
 }
