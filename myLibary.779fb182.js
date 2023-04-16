c56c6422fd6d72822af7172ed2abe344cbced458
PavloGram <113470072+PavloGram@users.noreply.github.com>
2023-04-16 07:02:16 +0000

    Deploying to gh-pages from @ PavloGram/project-group-13@d788c60cdaeccc921fe12d270ef5dd3c197b02cf 🚀

--- myLibary.779fb182.js
+++
@@ -1,2 +0,0 @@
-window.onload=function(){var e=document.body,d=e.querySelector(".preloader");e.classList.add("loaded_hiding"),d.addEventListener("transitionend",(function n(){e.classList.add("loaded"),e.classList.remove("loaded_hiding"),e.classList.remove("overflow_hidden"),d.removeEventListener("transitionend",n)}))};
-//# sourceMappingURL=myLibary.779fb182.js.map
