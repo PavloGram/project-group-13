dfdf1268cf2c739923ade406fe29115d0437217a
Tetiana Novikova <113465579+NovikovaTetiana@users.noreply.github.com>
2023-04-16 11:34:27 +0300

    add branch and code(needs to fix)

---
+++ src/popular-movies.js
@@ -0,0 +1,39 @@
+import axios from 'axios';
+// import { createCardsGallery } from './createCardGallery'; поміняти
+
+// const galleryEl = document.querySelector('.gallery'); замінити
+
+ class APIpopular {
+  #BASE_URL = 'https://api.themoviedb.org/3/';
+  #API_KEY = '1962278b5026dd7c7bb0a91cd47f798b';
+ 
+  constructor() {
+    this.page = 1;
+  }
+
+  async fetchMovies() {
+    try {
+      return await axios.get(`${this.#BASE_URL}trending/movie/week?api_key=${this.#API_KEY}`);
+    } catch (err) {
+      throw new Error(err.message);
+    }
+  }
+}
+
+const ApipopularMovies = new APIpopular();
+
+const handlerLoadWindow = () =>{
+
+  ApipopularMovies
+  .fetchMovies()
+  .then(({data})=>{
+console.log(data)
+
+// поміняти 
+// galleryEl.innerHTML = createCardsGallery(data.results);
+  }).catch(err => {
+    console.log(err)
+  })
+}
+
+window.addEventListener('load', handlerLoadWindow)
