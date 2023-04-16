fd6acbdc3fc2fc7f2cf34e977b48af980a3f8bc2
PavloGram <113470072+PavloGram@users.noreply.github.com>
2023-04-16 17:02:01 +0300

    і

--- src/pagination.js
+++ src/pagination.js
@@ -3,15 +3,15 @@ const BASE_URL_TRENDS = 'https://api.themoviedb.org/3/trending/all/day';
 const BASE_URL_QUERY = 'https://api.themoviedb.org/3/search/movie';
 const API_KEY = '1962278b5026dd7c7bb0a91cd47f798b';
 
-const inputEl = document.querySelector('.input');
+const inputEl = document.querySelector('.search__input');
 const spinnerEl = document.querySelector('.preloader__image');
 
 inputEl.addEventListener('input', inputQuery);
 let pageCounter = 1;
-let value = ''
+let value = '';
 
 function inputQuery(e) {
- value = e.target.value.trim();
+  value = e.target.value.trim();
 
   const searchParamsToQuery = new URLSearchParams({
     api_key: '1962278b5026dd7c7bb0a91cd47f798b',
@@ -19,18 +19,14 @@ function inputQuery(e) {
     page: pageCounter,
   });
   const url = `${BASE_URL_QUERY}?${searchParamsToQuery}`;
-     fetchFilms(url)
+  fetchFilms(url)
     .then(forMarcup => {
-
-          return      console.log(forMarcup);
-          })
+      return console.log(forMarcup);
+    })
     .catch(er => {
       console.log(er);
     });
 }
-
-
-
 
 function fetchFilms(url) {
   return fetch(url).then(response => {
@@ -41,17 +37,9 @@ function fetchFilms(url) {
   });
 }
 
-
-
-
-
-
-
-
-
 const paginationContainer = document.querySelector('.pagination');
 const paginationButtons =
-paginationContainer.querySelectorAll('.pagination-btn');
+  paginationContainer.querySelectorAll('.pagination-btn');
 const previousButton = paginationContainer.querySelector('.previous-btn');
 const nextButton = paginationContainer.querySelector('.next-btn');
 const endButton = paginationContainer.querySelector('.end-btn');
@@ -59,7 +47,6 @@ const nextDotsButton = paginationContainer.querySelector('.next-dots');
 
 let currentPage = 1;
 const totalPages = paginationButtons.length - 2;
-
 
 function displayCurrentPage() {
   paginationButtons.forEach(button => {
