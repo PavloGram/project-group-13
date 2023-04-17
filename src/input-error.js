a41cfa55e2c89083ee71aff694a36af6b99aa363
Ruslan <ruslan.grudistov@gmail.com>
2023-04-17 18:28:22 +0300

    2

--- src/input-error.js
+++ src/input-error.js
@@ -1,5 +1,5 @@
 export default function inputError(array) {
-  if (array.length > 0) {
+  if (array.length > 0 && !inputErrEl.contains('is-hidden')) {
     return inputErrEl.classList.add('is-hidden');
   }
   return inputErrEl.classList.remove('is-hidden');
