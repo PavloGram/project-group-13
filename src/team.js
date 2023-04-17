e385e83219a09cfc5a58750f474454857c2116a0
DimaSvirin <115728392+DimaSvirin@users.noreply.github.com>
2023-04-17 12:07:31 +0300

    animated heart and fix team images

--- src/team.js
+++ src/team.js
@@ -1,62 +1,80 @@
+import iconTemlead from './images/team/icon-teamlead.png';
+import iconScrum from './images/team/icon-scrum.png';
+import iconMale from './images/team/icon-male.png';
+import iconFemale from './images/team/icon-female.png';
+
+import member1 from './images/team/Pavlo_Gramazhora.jpg';
+import member2 from './images/team/Tetiana_Novikova.jpeg';
+import member3 from './images/team/Volodymyr_Medvid.jpg';
+import member4 from './images/team/Dmytro_Svirin.jpg';
+import member5 from './images/team/Iryna_Vakulenko.jpg';
+import member6 from './images/team/Oleh_Zhuravskyi.jpg';
+import member7 from './images/team/Ruslana_Semenenko.jpg';
+import member8 from './images/team/Yuriy_Berketa.jpg';
+import member9 from './images/team/Vadim_Khvostikov.jpg';
+import member10 from './images/team/Ruslan_Grudistov.jpg';
+
+
+
 export const teamMembers = [
     {
         name: 'Pavlo Gramazhora',
-        icon: './images/team/icon-teamlead.png',
-        photo: './images/team/Pavlo_Gramazhora.jpg',
+        icon: iconTemlead,
+        photo: member1,
         work: 'Team Lead, HTML, CSS, Java Script',
     },
     {
         name: 'Tetiana Novikova',
-        icon: './images/team/icon-scrum.png',
-        photo: './images/team/Tetiana_Novikova.jpeg',
+        icon: iconScrum,
+        photo: member2,
         work: 'Scrum Master, HTML, CSS, Java Script',
     },
     {
-        name: 'Iryna Vakulenko',
-        icon: './images/team/icon-female.png',
-        photo: './images/team/Iryna_Vakulenko.jpg',
-        work: 'HTML, CSS, Java Script',
-    },
-    {
-        name: 'Oleh Zhuravskyi',
-        icon: './images/team/icon-male.png',
-        photo: './images/team/Oleh_Zhuravskyi.jpg',
-        work: 'HTML, CSS, Java Script',
-    },
-    {
         name: 'Volodymyr Medvid',
-        icon: './images/team/icon-male.png',
-        photo: './images/team/Volodymyr_Medvid.jpg',
-        work: 'HTML, CSS, Java Script',
-    },
-    {
-        name: 'Ruslana Semenenko',
-        icon: './images/team/icon-female.png',
-        photo: './images/team/Ruslana_Semenenko.jpg',
+        icon: iconMale,
+        photo: member3,
         work: 'HTML, CSS, Java Script',
     },
     {
         name: 'Dmytro Svirin',
-        icon: './images/team/icon-male.png',
-        photo: './images/team/Dmytro_Svirin.jpg',
+        icon: iconMale,
+        photo: member4,
         work: 'HTML, CSS, Java Script',
     },
     {
-        name: 'Ruslan Grudistov',
-        icon: './images/team/icon-male.png',
-        photo: './images/team/Ruslan_Grudistov.jpg',
+        name: 'Iryna Vakulenko',
+        icon: iconFemale,
+        photo: member5,
+        work: 'HTML, CSS, Java Script',
+    },
+    {
+        name: 'Oleh Zhuravskyi',
+        icon: iconMale,
+        photo: member6,
+        work: 'HTML, CSS, Java Script',
+    }, 
+    {
+        name: 'Ruslana Semenenko',
+        icon: iconFemale,
+        photo: member7,
         work: 'HTML, CSS, Java Script',
     },
     {
         name: 'Yuriy Berketa',
-        icon: './images/team/icon-male.png',
-        photo: './images/team/Yuriy_Berketa.jpg',
+        icon: iconMale,
+        photo: member8,
         work: 'HTML, CSS, Java Script',
     },
     {
         name: 'Vadim Khvostikov',
-        icon: './images/team/icon-male.png',
-        photo: './images/team/Vadim_Khvostikov.JPG',
+        icon: iconMale,
+        photo: member9,
+        work: 'HTML, CSS, Java Script',
+    },
+    {
+        name: 'Ruslan Grudistov',
+        icon: iconMale,
+        photo: member10,
         work: 'HTML, CSS, Java Script',
     },
 ]
