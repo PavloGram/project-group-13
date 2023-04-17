ac968f76ec27e3e27f9f29770d66d11782d1e8cc
PavloGram <113470072+PavloGram@users.noreply.github.com>
2023-04-17 17:36:15 +0300

    в

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import './spinner.js';
 import './fetchFilms.js';
-// import './pagination.js';
+import './pagination.js';
 import './popular-movies.js';
 import './js/render-markup.js';
 
--- src/pagination.js
+++ src/pagination.js
@@ -1,4 +1,5 @@
 // import { fetchFilms } from './fetchFilms.js';
+import { renderMarkup } from './js/render-markup';
 const BASE_URL_TRENDS = 'https://api.themoviedb.org/3/trending/all/day';
 const BASE_URL_QUERY = 'https://api.themoviedb.org/3/search/movie';
 const API_KEY = '1962278b5026dd7c7bb0a91cd47f798b';
@@ -24,7 +25,8 @@ function inputQuery(e) {
   fetchFilms(url)
     .then(forMarcup => {
       totalResults = forMarcup.total_results;
-      // return console.log(totalResults);
+      console.log(forMarcup)
+      renderMarkup(forMarcup);
     })
     .catch(er => {
       console.log(er);
@@ -61,62 +63,62 @@ function fetchFilms(url) {
 
 // <----------------->
 
-const paginationContainer = document.querySelector('.pagination');
-const paginationButtons =
-  paginationContainer.querySelectorAll('.pagination-btn');
-const previousButton = paginationContainer.querySelector('.previous-btn');
-const nextButton = paginationContainer.querySelector('.next-btn');
-const endButton = paginationContainer.querySelector('.end-btn');
-const nextDotsButton = paginationContainer.querySelector('.next-dots');
+// const paginationContainer = document.querySelector('.pagination');
+// const paginationButtons =
+//   paginationContainer.querySelectorAll('.pagination-btn');
+// const previousButton = paginationContainer.querySelector('.previous-btn');
+// const nextButton = paginationContainer.querySelector('.next-btn');
+// const endButton = paginationContainer.querySelector('.end-btn');
+// const nextDotsButton = paginationContainer.querySelector('.next-dots');
 
-let currentPage = 1;
-const totalPages = paginationButtons.length - 2;
+// let currentPage = 1;
+// const totalPages = paginationButtons.length - 2;
 
-function displayCurrentPage() {
-  paginationButtons.forEach(button => {
-    button.classList.remove('active-pagination');
-    if (button.innerText == currentPage) {
-      button.classList.add('active-pagination');
-    }
-  });
-}
+// function displayCurrentPage() {
+//   paginationButtons.forEach(button => {
+//     button.classList.remove('active-pagination');
+//     if (button.innerText == currentPage) {
+//       button.classList.add('active-pagination');
+//     }
+//   });
+// }
 
-paginationButtons.forEach(button => {
-  button.addEventListener('click', () => {
-    if (button.innerText != '...' && currentPage != button.innerText) {
-      currentPage = parseInt(button.innerText);
-      displayCurrentPage();
-    }
-  });
-});
+// paginationButtons.forEach(button => {
+//   button.addEventListener('click', () => {
+//     if (button.innerText != '...' && currentPage != button.innerText) {
+//       currentPage = parseInt(button.innerText);
+//       displayCurrentPage();
+//     }
+//   });
+// });
 
-previousButton.addEventListener('click', () => {
-  if (currentPage > 1) {
-    currentPage--;
-    displayCurrentPage();
-  }
-});
+// previousButton.addEventListener('click', () => {
+//   if (currentPage > 1) {
+//     currentPage--;
+//     displayCurrentPage();
+//   }
+// });
 
-nextButton.addEventListener('click', () => {
-  if (currentPage < totalPages) {
-    currentPage++;
-    displayCurrentPage();
-  }
-});
+// nextButton.addEventListener('click', () => {
+//   if (currentPage < totalPages) {
+//     currentPage++;
+//     displayCurrentPage();
+//   }
+// });
 
-endButton.addEventListener('click', () => {
-  if (currentPage != totalPages) {
-    currentPage = totalPages;
-    displayCurrentPage();
-  }
-});
+// endButton.addEventListener('click', () => {
+//   if (currentPage != totalPages) {
+//     currentPage = totalPages;
+//     displayCurrentPage();
+//   }
+// });
 
-nextDotsButton.addEventListener('click', () => {
-  if (currentPage < totalPages - 2) {
-    currentPage += 3;
-    displayCurrentPage();
-  } else {
-    currentPage = totalPages;
-    displayCurrentPage();
-  }
-});
+// nextDotsButton.addEventListener('click', () => {
+//   if (currentPage < totalPages - 2) {
+//     currentPage += 3;
+//     displayCurrentPage();
+//   } else {
+//     currentPage = totalPages;
+//     displayCurrentPage();
+//   }
+// });
