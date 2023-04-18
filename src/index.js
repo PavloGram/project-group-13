d2033187beec546b169e91ada49f1a70edb6b02b
PavloGram <113470072+PavloGram@users.noreply.github.com>
2023-04-18 08:33:08 +0300

    п

--- src/index.js
+++ src/index.js
@@ -3,6 +3,6 @@ import './fetchFilms.js';
 import './pagination.js';
 import './popular-movies.js';
 import './modal-team.js'
-import './get_localstorage_info.js'
+
 // import './js/render-markup.js';
 
--- src/myLibary.js
+++ src/myLibary.js
@@ -1,2 +1,3 @@
 import './spinner'
 import './modal-team.js'
+import './get_localstorage_info.js'
