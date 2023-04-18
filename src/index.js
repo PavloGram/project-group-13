188a484ee93ffefeebf5c00bbd4b4861c4f7dd79
PavloGram <113470072+PavloGram@users.noreply.github.com>
2023-04-18 18:17:56 +0300

    footer

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
-import './js/spinner';
-import './js/fetchFilms';
-import './js/pagination';
-import './js/popular-movies';
+import './js/spinner.js';
+import './js/fetchFilms.js';
+import './js/pagination.js';
+import './js/popular-movies.js';
 import './js/modal-team.js'
+import './js/modal-about-movie.js'
 // import './spinner.js'
 // import './js/render-markup.js';
 
--- src/myLibary.js
+++ src/myLibary.js
@@ -1,4 +1,4 @@
-import './js/spinner'
+import './js/spinner.js'
 import './js/modal-team.js'
 import './js/get_localstorage_info.js'
 import './js/modal-about-movie.js'
