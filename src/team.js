163bb9c9f73ec07a0dbb4050b7f54d1533d20f1f
PavloGram <113470072+PavloGram@users.noreply.github.com>
2023-04-18 09:21:26 +0300

    d

--- src/team.js
+++ src/team.js
@@ -1,18 +1,18 @@
-import iconTemlead from './images/team/icon-teamlead.png';
-import iconScrum from './images/team/icon-scrum.png';
-import iconMale from './images/team/icon-male.png';
-import iconFemale from './images/team/icon-female.png';
+// import iconTemlead from './images/team/icon-teamlead.png';
+// import iconScrum from './images/team/icon-scrum.png';
+// import iconMale from './images/team/icon-male.png';
+// import iconFemale from './images/team/icon-female.png';
 
-import member1 from './images/team/Pavlo_Gramazhora.jpg';
-import member2 from './images/team/Tetiana_Novikova.jpg';
-import member3 from './images/team/Volodymyr_Medvid.jpg';
-import member4 from './images/team/Dmytro_Svirin.jpg';
-import member5 from './images/team/Iryna_Vakulenko.jpg';
-import member6 from './images/team/Oleh_Zhuravskyi.jpg';
-import member7 from './images/team/Ruslana_Semenenko.jpg';
-import member8 from './images/team/Yuriy_Berketa.jpg';
-import member9 from './images/team/Vadim_Khvostikov.jpg';
-import member10 from './images/team/Ruslan_Grudistov.jpg';
+// import member1 from './images/team/Pavlo_Gramazhora.jpg';
+// import member2 from './images/team/Tetiana_Novikova.jpg';
+// import member3 from './images/team/Volodymyr_Medvid.jpg';
+// import member4 from './images/team/Dmytro_Svirin.jpg';
+// import member5 from './images/team/Iryna_Vakulenko.jpg';
+// import member6 from './images/team/Oleh_Zhuravskyi.jpg';
+// import member7 from './images/team/Ruslana_Semenenko.jpg';
+// import member8 from './images/team/Yuriy_Berketa.jpg';
+// import member9 from './images/team/Vadim_Khvostikov.jpg';
+// import member10 from './images/team/Ruslan_Grudistov.jpg';
 
 
 export const teamMembers = [
