8172dc735eebe87e40324fe8677089e9b736cf2f
PavloGram <113470072+PavloGram@users.noreply.github.com>
2023-04-19 07:54:07 +0300

    red

--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
 import './js/spinner.js';
 import './js/fetchFilms.js';
-import './js/pagination.js';
 import './js/popular-movies.js';
 import './js/modal-team.js'
 import './js/modal-about-movie.js'
 import './js/pag.js'
-// import './spinner.js'
-// import './js/render-markup.js';
+
 
